41490ab6e804b99e989290b9781ec03cce2fd30f
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-24 14:52:48 -0300

    Creating repository

---
+++ backend/src/main/java/com/example/cursos_backend/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.cursos_backend.repositories;
+
+import com.example.cursos_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+}
