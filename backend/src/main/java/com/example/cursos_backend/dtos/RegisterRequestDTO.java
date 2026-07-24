17070d101e59e58e48b9fb54c111e549004fcdd8
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-24 14:52:05 -0300

    Creating DTO

---
+++ backend/src/main/java/com/example/cursos_backend/dtos/RegisterRequestDTO.java
@@ -0,0 +1,9 @@
+package com.example.cursos_backend.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequestDTO(@NotBlank String name,
+                                 @NotBlank @Email String email,
+                                 @NotBlank @Size(min = 8) String password) {}
