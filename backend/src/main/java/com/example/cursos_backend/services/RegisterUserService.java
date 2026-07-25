c9aafe3cff5ade7221a1b7081b342de5ae2f6ba7
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-25 14:31:03 -0300

    Updating registerUser

--- backend/src/main/java/com/example/cursos_backend/services/RegisterUserService.java
+++ backend/src/main/java/com/example/cursos_backend/services/RegisterUserService.java
@@ -1,6 +1,7 @@
 package com.example.cursos_backend.services;
 
 import com.example.cursos_backend.dtos.RegisterRequestDTO;
+import com.example.cursos_backend.dtos.RegisterResponseDTO;
 import com.example.cursos_backend.enums.Role;
 import com.example.cursos_backend.exceptions.EmailAlreadyExistsException;
 import com.example.cursos_backend.model.User;
@@ -16,7 +17,7 @@ public class RegisterUserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void registerUser(RegisterRequestDTO registerRequestDTO) {
+    public RegisterResponseDTO registerUser(RegisterRequestDTO registerRequestDTO) {
 
         if(userRepository.existsByEmail(registerRequestDTO.email())) {
             throw new EmailAlreadyExistsException();
@@ -30,6 +31,12 @@ public class RegisterUserService {
         String encodedPassword = passwordEncoder.encode(registerRequestDTO.password());
         user.setPassword(encodedPassword);
 
-        userRepository.save(user);
+        User newUser = userRepository.save(user);
+
+        return new RegisterResponseDTO(
+                newUser.getId(),
+                newUser.getName(),
+                newUser.getEmail(),
+                newUser.getRole());
     }
 }
