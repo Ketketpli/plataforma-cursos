b02022793183960eaab6c674394fc3f6d374bb11
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-27 17:11:52 -0300

    Adding loginUser endpoint

--- backend/src/main/java/com/example/cursos_backend/controller/UserAuthController.java
+++ backend/src/main/java/com/example/cursos_backend/controller/UserAuthController.java
@@ -1,7 +1,10 @@
 package com.example.cursos_backend.controller;
 
+import com.example.cursos_backend.dtos.LoginRequestDTO;
+import com.example.cursos_backend.dtos.LoginResponseDTO;
 import com.example.cursos_backend.dtos.RegisterRequestDTO;
 import com.example.cursos_backend.dtos.RegisterResponseDTO;
+import com.example.cursos_backend.services.LoginUserService;
 import com.example.cursos_backend.services.RegisterUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +15,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-public class UserRegisterController {
+public class UserAuthController {
 
     private final RegisterUserService registerUserService;
+    private final LoginUserService loginUserService;
 
     @PostMapping(value = "/register")
     public ResponseEntity<RegisterResponseDTO> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
         RegisterResponseDTO newUser = registerUserService.registerUser(registerRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<LoginResponseDTO> loginUser(@Valid @RequestBody LoginRequestDTO loginRequestDTO){
+        String userToken = loginUserService.loginUser(loginRequestDTO);
+        LoginResponseDTO logUser = new LoginResponseDTO(userToken);
+        return ResponseEntity.status(HttpStatus.OK).body(logUser);
     }
 }
