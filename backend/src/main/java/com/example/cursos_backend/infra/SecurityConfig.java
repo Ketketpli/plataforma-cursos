2931a27cb8bb0993157d53e8f3f7ddfa1e8b3a41
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-27 17:12:27 -0300

    Adding login permission

--- backend/src/main/java/com/example/cursos_backend/infra/SecurityConfig.java
+++ backend/src/main/java/com/example/cursos_backend/infra/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
