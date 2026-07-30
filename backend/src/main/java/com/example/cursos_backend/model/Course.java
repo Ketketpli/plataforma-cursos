cb685aa9b40aceb572ad8119706c4bdb6d16142f
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-30 16:55:47 -0300

    Creating course model

---
+++ backend/src/main/java/com/example/cursos_backend/model/Course.java
@@ -0,0 +1,47 @@
+package com.example.cursos_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "courses")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_seq")
+    @SequenceGenerator(name = "course_seq", sequenceName = "course_seq", allocationSize = 1)
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "course_category",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User instructor;
+}
