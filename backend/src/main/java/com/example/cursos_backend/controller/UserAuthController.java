859a7096cfd20bf4f61bfd639316e3e2aee93102
Paulo Henrique <varelapaulo852@gmail.com>
2026-07-30 15:02:55 -0300

    Adding /me endpoint

--- backend/src/main/java/com/example/cursos_backend/controller/UserAuthController.java
+++ backend/src/main/java/com/example/cursos_backend/controller/UserAuthController.java
@@ -4,12 +4,15 @@ import com.example.cursos_backend.dtos.LoginRequestDTO;
 import com.example.cursos_backend.dtos.LoginResponseDTO;
 import com.example.cursos_backend.dtos.RegisterRequestDTO;
 import com.example.cursos_backend.dtos.RegisterResponseDTO;
+import com.example.cursos_backend.model.User;
 import com.example.cursos_backend.services.LoginUserService;
 import com.example.cursos_backend.services.RegisterUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,5 +34,11 @@ public class UserAuthController {
         String userToken = loginUserService.loginUser(loginRequestDTO);
         LoginResponseDTO logUser = new LoginResponseDTO(userToken);
         return ResponseEntity.status(HttpStatus.OK).body(logUser);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<RegisterResponseDTO> me(@AuthenticationPrincipal User user) {
+        RegisterResponseDTO response = new RegisterResponseDTO(user.getId(), user.getName(), user.getEmail(), user.getRole());
+        return ResponseEntity.ok(response);
     }
 }
