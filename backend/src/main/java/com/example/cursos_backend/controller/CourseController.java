2f1f1ddfe1b13be11632e570bf053e1d6cd7b8fe
Paulo Henrique <varelapaulo852@gmail.com>
2026-08-06 21:31:11 -0300

    Implementing CRUD

--- backend/src/main/java/com/example/cursos_backend/controller/CourseController.java
+++ backend/src/main/java/com/example/cursos_backend/controller/CourseController.java
@@ -6,14 +6,15 @@ import com.example.cursos_backend.model.User;
 import com.example.cursos_backend.services.CourseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/courses")
@@ -25,7 +26,33 @@ public class CourseController {
     @PreAuthorize("hasRole('INSTRUCTOR')")
     @PostMapping
     public ResponseEntity<CourseResponseDTO> createCourse(@Valid @RequestBody CourseRequestDTO request, @AuthenticationPrincipal User user) {
-        CourseResponseDTO newCourse = courseService.createCourse(request, user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newCourse);
+
+        CourseResponseDTO course = courseService.createCourse(request, user);
+
+        URI location = URI.create("/course/" + course.id());
+
+        return ResponseEntity.created(location).body(course);
+    }
+
+    @PreAuthorize("hasAnyRole('INSTRUCTOR', 'ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<CourseResponseDTO> updateCourse(@PathVariable Long id, @Valid @RequestBody CourseRequestDTO request, @AuthenticationPrincipal User user) {
+
+        return ResponseEntity.ok(courseService.updateCourse(id, request, user));
+    }
+
+    @PreAuthorize("hasAnyRole('INSTRUCTOR', 'ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id, @AuthenticationPrincipal User user) {
+
+        courseService.deleteCourse(id, user);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CourseResponseDTO>> getAllCourses(Pageable pageable) {
+
+        return ResponseEntity.ok(courseService.getAllCourses(pageable));
     }
 }
