ba650f31d176f59ea7a2ae40f21e80a4e5050c8b
Paulo Henrique <varelapaulo852@gmail.com>
2026-08-14 14:05:03 -0300

    Setting up Swagger

--- backend/src/main/java/com/example/cursos_backend/infra/SecurityConfig.java
+++ backend/src/main/java/com/example/cursos_backend/infra/SecurityConfig.java
@@ -34,6 +34,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
