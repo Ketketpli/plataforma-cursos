e2d4f8974c2b7d2e698d2297811a745e520b8435
Paulo Henrique <varelapaulo852@gmail.com>
2026-08-15 15:02:03 -0300

    Creating Enrollment service

---
+++ backend/src/main/java/com/example/cursos_backend/services/EnrollmentService.java
@@ -0,0 +1,44 @@
+package com.example.cursos_backend.services;
+
+import com.example.cursos_backend.dtos.EnrollResponseDTO;
+import com.example.cursos_backend.exceptions.EnrollmentAlreadyExistException;
+import com.example.cursos_backend.exceptions.ValueNotFoundException;
+import com.example.cursos_backend.infra.AuthorizationHelper;
+import com.example.cursos_backend.model.Course;
+import com.example.cursos_backend.model.Enrollment;
+import com.example.cursos_backend.model.User;
+import com.example.cursos_backend.repositories.CourseRepository;
+import com.example.cursos_backend.repositories.EnrollmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class EnrollmentService {
+
+    private final CourseRepository courseRepository;
+    private final EnrollmentRepository enrollmentRepository;
+    private final AuthorizationHelper authorizationHelper;
+
+    public EnrollResponseDTO enroll(Long courseId, User student) {
+
+        Course course = courseRepository.findById(courseId).orElseThrow(() -> new ValueNotFoundException("Curso não encontrado"));
+        authorizationHelper.checkNotOwner(course.getInstructor().getId(), student);
+        if (enrollmentRepository.existsByStudentIdAndCourseId(student.getId(), courseId)) {
+            throw new EnrollmentAlreadyExistException("Aluno já matriculado");
+        }
+
+        Enrollment enrollment = new Enrollment();
+        enrollment.setCourse(course);
+        enrollment.setStudent(student);
+        enrollment.setEnrolledAt(LocalDateTime.now());
+        enrollmentRepository.save(enrollment);
+
+        return new EnrollResponseDTO(enrollment.getId(),
+                enrollment.getCourse().getId(),
+                enrollment.getStudent().getId(),
+                enrollment.getEnrolledAt());
+    }
+}
