135c5be6580b5f32f282195bd318259eb3788fd9
Paulo Henrique <varelapaulo852@gmail.com>
2026-08-15 13:45:31 -0300

    Setting up Swagger

--- backend/src/main/java/com/example/cursos_backend/infra/SecurityConfig.java
+++ backend/src/main/java/com/example/cursos_backend/infra/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(
+                                "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
