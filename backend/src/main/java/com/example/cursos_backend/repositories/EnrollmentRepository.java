e1a61d5d58dee2fccb0b2f0317e2de697d914958
Paulo Henrique <varelapaulo852@gmail.com>
2026-08-21 17:35:18 -0300

    Implementing findByStudentIdAndCourseId

--- backend/src/main/java/com/example/cursos_backend/repositories/EnrollmentRepository.java
+++ backend/src/main/java/com/example/cursos_backend/repositories/EnrollmentRepository.java
@@ -4,10 +4,13 @@ import com.example.cursos_backend.model.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
     boolean existsByStudentIdAndCourseId(Long studentId, Long courseId);
 
     List<Enrollment> findByStudentId(Long studentId);
+
+    Optional<Enrollment> findByStudentIdAndCourseId(Long studentId, Long courseId);
 }
