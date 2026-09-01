fd9f8b35c08ca609a46a360232f1b2ee43f3633d
Paulo Henrique <varelapaulo852@gmail.com>
2026-09-01 20:09:43 -0300

    Adding findAverageRatingByCourseId

--- backend/src/main/java/com/example/cursos_backend/repositories/ReviewRepository.java
+++ backend/src/main/java/com/example/cursos_backend/repositories/ReviewRepository.java
@@ -4,8 +4,15 @@ import com.example.cursos_backend.model.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findByEnrollmentCourseId(Long courseId, Pageable pageable);
+
+    @Query("SELECT AVG(r.rating) FROM Review r WHERE r.enrollment.course.id = :courseId")
+    Double findAverageRatingByCourseId(@Param("courseId") Long courseId);
+
+    long countByCourseId(Long courseId);
 }
