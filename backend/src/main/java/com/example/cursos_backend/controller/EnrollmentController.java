48dcd0b97ae762eaa106c2553b49bcf037b135f4
Paulo Henrique <varelapaulo852@gmail.com>
2026-09-01 20:11:09 -0300

    Adding getMyCourses endpoint

--- backend/src/main/java/com/example/cursos_backend/controller/EnrollmentController.java
+++ backend/src/main/java/com/example/cursos_backend/controller/EnrollmentController.java
@@ -1,29 +1,36 @@
 package com.example.cursos_backend.controller;
 
 import com.example.cursos_backend.dtos.EnrollResponseDTO;
+import com.example.cursos_backend.dtos.StudentCourseDashboardDTO;
 import com.example.cursos_backend.model.User;
 import com.example.cursos_backend.services.EnrollmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/courses/{courseId}/enroll")
 @RequiredArgsConstructor
 public class EnrollmentController {
 
     private final EnrollmentService enrollmentService;
 
-    @PostMapping
+    @PostMapping("/courses/{courseId}/enroll")
     public ResponseEntity<EnrollResponseDTO> enroll(@PathVariable Long courseId, @AuthenticationPrincipal User student) {
 
         EnrollResponseDTO newEnroll = enrollmentService.enroll(courseId, student);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(newEnroll);
+    }
+
+    @GetMapping("/students/me/courses")
+    public ResponseEntity<List<StudentCourseDashboardDTO>> getMyCourses (@AuthenticationPrincipal User student) {
+
+        List<StudentCourseDashboardDTO> enrollments = enrollmentService.getMyCourses(student);
+
+        return ResponseEntity.ok(enrollments);
     }
 }
